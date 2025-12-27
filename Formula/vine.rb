85ed34c3c5a1f03be01bf56e69e059e2023100d5
Adam Siepel <asiepel@cshl.edu>
2025-12-26 19:45:07 -0500

    another typo

--- Formula/vine.rb
+++ Formula/vine.rb
@@ -1,7 +1,7 @@
 class Vine < Formula
   desc "Variational Inference with Node Embedding"
   homepage "https://github.com/CshlSiepelLab/vine"
-  url "https://github.com/CshlSiepelLab/vine/archive/refs/tags/0.2.0.tar.gz"
+  url "https://github.com/CshlSiepelLab/vine/archive/refs/tags/v0.2.0.tar.gz"
   sha256 "5b2034bb4d6ac5a3bdfc0ead45db27bde9d5845225c7c3f63c15927b0681fe3f"
   license "BSD-3-Clause"
 
