56566d8dc255ee22f32eebb47223f5c4c843c5de
Adam Siepel <asiepel@cshl.edu>
2026-03-13 16:59:48 -0400

    updated for v0.3.4

--- Formula/vine.rb
+++ Formula/vine.rb
@@ -1,8 +1,8 @@
 class Vine < Formula
   desc "Variational Inference with Node Embedding"
   homepage "https://github.com/CshlSiepelLab/vine"
-  url "https://github.com/CshlSiepelLab/vine/archive/refs/tags/v0.3.3.tar.gz"
-  sha256 "fafd718cab214f78c1cb529a16f1f5c908e4dd8f40aeed0d8c90ef4192a68913"
+  url "https://github.com/CshlSiepelLab/vine/archive/refs/tags/v0.3.4.tar.gz"
+  sha256 "b1992ac7c9bd5515b2ae7857bbb559c830b5e35c6e82fb73e3dcf29cca9329e9"
   license "BSD-3-Clause"
 
   depends_on "cmake" => :build
