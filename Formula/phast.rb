93e885068b2c496e2bbadad9815d42f5d8459cb4
Adam Siepel <asiepel@cshl.edu>
2026-07-25 17:45:53 -0400

    phast: update to v1.9.9 (explicit libm link)

--- Formula/phast.rb
+++ Formula/phast.rb
@@ -1,8 +1,8 @@
 class Phast < Formula
   desc "Phylogenetic Analysis with Space/Time models"
   homepage "https://github.com/CshlSiepelLab/phast"
-  url "https://github.com/CshlSiepelLab/phast/archive/refs/tags/v1.9.8.tar.gz"
-  sha256 "131e562215251768844fd21b9334e99758c3e2a0847a3956d69fc1c5c1f585c7"
+  url "https://github.com/CshlSiepelLab/phast/archive/refs/tags/v1.9.9.tar.gz"
+  sha256 "ef9fd7c6e6c02e866162cf41ff372559f105ae3f9c4dc8562103533e41f1b069"
   license "BSD-3-Clause"
 
   depends_on "cmake" => :build
