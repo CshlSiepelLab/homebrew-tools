e18c50edcba079462a4fca994db0a5f16a6bb4ad
Adam Siepel <asiepel@cshl.edu>
2026-07-25 16:00:21 -0400

    vine: update to v0.3.5 (portable BLAS build fix)

--- Formula/vine.rb
+++ Formula/vine.rb
@@ -2,7 +2,7 @@ class Vine < Formula
   desc "Variational Inference with Node Embedding"
   homepage "https://github.com/CshlSiepelLab/vine"
   url "https://github.com/CshlSiepelLab/vine/archive/refs/tags/v0.3.5.tar.gz"
-  sha256 "8eb4e5359ac2afc55894118c6d3399efac225e66fde9eb16b96742c213035414"
+  sha256 "874a2a87b4937ee626c5c0381ab71301960d4af051650b46aba156d8b9f7967a"
   license "BSD-3-Clause"
 
   depends_on "cmake" => :build
