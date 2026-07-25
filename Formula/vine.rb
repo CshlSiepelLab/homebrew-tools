1190361ca0ad8f705310a3bd734b21649c323f15
Adam Siepel <asiepel@cshl.edu>
2026-07-25 16:04:35 -0400

    vine: bump sha for opt-in BLAS thread control fix

--- Formula/vine.rb
+++ Formula/vine.rb
@@ -2,7 +2,7 @@ class Vine < Formula
   desc "Variational Inference with Node Embedding"
   homepage "https://github.com/CshlSiepelLab/vine"
   url "https://github.com/CshlSiepelLab/vine/archive/refs/tags/v0.3.5.tar.gz"
-  sha256 "874a2a87b4937ee626c5c0381ab71301960d4af051650b46aba156d8b9f7967a"
+  sha256 "9ec46b057c1cd36fc2dda36f9b93e51a1acdb922caa86bfaba29af99c5c33253"
   license "BSD-3-Clause"
 
   depends_on "cmake" => :build
